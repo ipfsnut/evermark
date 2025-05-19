641ffb5f61c15c8d6e417e328df61d9b174a8a3e
ipfsnut <40326908+ipfsnut@users.noreply.github.com>
2025-05-19 18:42:54 -0500

    pulled backend code. big update coming.

--- src/config/supabase.ts
+++
@@ -1,7 +0,0 @@
-import { createClient } from '@supabase/supabase-js';
-import { API_CONFIG } from './constants';
-
-export const supabase = createClient(
-  API_CONFIG.SUPABASE_URL,
-  API_CONFIG.SUPABASE_ANON_KEY
-);
